c94ad34df3b803798aae4a0336b9fc27e12eadd8
wangdaogang <1229706777@qq.com>
2019-09-08 00:47:52 +0800

    update spring container bean init

--- spring-core/src/main/java/com/wdg/context/ApplicationContext.java
+++ spring-core/src/main/java/com/wdg/context/ApplicationContext.java
@@ -14,44 +14,60 @@ import java.util.*;
 
 public class ApplicationContext {
 
-
     private static Document config;
+
+    private static Map<String,Object> singleIdBeanMap = new HashMap<>();
+    private static Map<Class,Object> singleClassBeanMap = new HashMap<>();
 
 
     static {
         SAXReader saxReader = new SAXReader();
         try {
-
             config = saxReader.read("/Users/wangdaogang/workspace/all-aspect-study/spring-core/src/main/resources/applicationContext.xml");
+            refreshContext(config);
         } catch (DocumentException e) {
             e.printStackTrace();
         }
     }
 
-    //1.
-    public Object getBean(String beanId) throws Exception {
-        Assert.notNull(beanId,"beanId must not be null");
+    private static void refreshContext(Document config) {
         Element rootElement = config.getRootElement();
         Iterator iterator = rootElement.elementIterator();
-        while (iterator.hasNext()){
+        while (iterator.hasNext()) {
             Element element = (Element) iterator.next();
             String idValue = element.attributeValue(BeanConstans.BEAN_ID);
-            if(beanId.equals(idValue)){
-                Class<?> aClass = Class.forName(element.attributeValue(BeanConstans.BEAN_CLASS));
+            Class<?> aClass = null;
+            try {
+                aClass = Class.forName(element.attributeValue(BeanConstans.BEAN_CLASS));
                 Object o = aClass.newInstance();
                 List<Element> propertys = element.elements(BeanConstans.BEAN_PROPERTY);
-                if(Objects.nonNull(propertys) || !propertys.isEmpty()){
+                if (Objects.nonNull(propertys) || !propertys.isEmpty()) {
                     Map<String, Object> map = getBeanProperty(propertys);
                     reflectSetProperty(o, aClass, map);
                 }
-                return o;
+                singleIdBeanMap.put(idValue, o);
+                singleClassBeanMap.put(aClass,o);
+            } catch (Exception e) {
+                e.printStackTrace();
             }
-
         }
-        return null;
+    }
+    //1.
+    public Object getBean(String beanId) throws Exception {
+        Assert.notNull(beanId,"beanId must not be null");
+        Object o = singleIdBeanMap.get(beanId);
+        if(Objects.isNull(o)){
+            System.out.println("容器");
+        }
+        return singleIdBeanMap.get(beanId);
     }
 
-    private Map<String,Object> getBeanProperty(List<Element> propertys){
+    public Object getBean(Class aClass){
+        Assert.notNull(aClass,"aClass must not be null");
+        return singleClassBeanMap.get(aClass);
+    }
+
+    private static Map<String,Object> getBeanProperty(List<Element> propertys){
         Map<String,Object> map = new HashMap<>();
         propertys.stream().forEach(property->
         {
@@ -63,7 +79,7 @@ public class ApplicationContext {
         return map;
     }
 
-    private void reflectSetProperty(Object target,Class aClass,Map<String,Object> propertyMap) throws Exception {
+    private static  void reflectSetProperty(Object target,Class aClass,Map<String,Object> propertyMap) throws Exception {
         Field[] fields = aClass.getDeclaredFields();
         //判断属性值 是否存在
         for(Field field : fields){
